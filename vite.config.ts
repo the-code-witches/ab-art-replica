6f533d59b9b4701e57697fa6dc42a784087ef468
Denise Sengül <denise@thecodewitches.de>
2026-03-11 10:15:08 +0100

    Update vite.config.ts to serve github pages domain

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/ab-art-replica/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
